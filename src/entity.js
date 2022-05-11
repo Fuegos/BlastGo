77621d300285936fc600c31cbc7d19a58d56de3e
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-05-12 03:32:39 +0400

    Update entity.js

--- src/entity.js
+++ src/entity.js
@@ -1,20 +1,41 @@
 import { Sprite } from "pixi.js";
 
+
 export class Entity {
-    constructor(texture, valueFill) {
+    constructor(texture, valueFill, parent, indentTop, indentLeft) {
         this.sprite = new Sprite(texture, valueFill);
         this.originalWidth = texture.width;
         this.originalHeight = texture.height;
+
+        this.parent = parent;
+
         this.valueFill = valueFill;
+        this.indentTop = indentTop;
+        this.indentLeft = indentLeft;
     }
 
-    drawing = () => this.sprite;
+    getSprite = () => this.sprite;
 
-    resize = (parentWidth = this.sprite.parent.width, parentHeight = this.sprite.parent.height) => {        
-        this.sprite.width = Math.round(parentWidth * this.valueFill);
+    getParentWidth = () => this.parent ? this.parent.width : window.innerWidth; 
+
+    getParentHeight = () => this.parent ? this.parent.height : window.innerHeight; 
+
+    getParentX = () => this.parent ? this.parent.x : 0;
+
+    getParentY = () => this.parent ? this.parent.y : 0;
+
+    resize = () => {        
+        this.sprite.width = Math.round(this.getParentWidth() * this.valueFill);
         let delta = this.sprite.width / this.originalWidth;
         this.sprite.height = Math.round(this.originalHeight * delta);
 
-        console.log(parentWidth, parentHeight, delta, this.sprite.width, this.sprite.height);
+        //console.log(this.sprite.width, this.sprite.height, this.getParentWidth(), this.sprite.texture.textureCacheIds[0]);
+    }
+
+    setPosition = () => {
+        this.sprite.x = this.getParentX() + Math.round(this.getParentWidth() * this.indentLeft);
+        this.sprite.y = this.getParentY() + Math.round(this.getParentHeight() * this.indentTop); 
+
+        //console.log(this.sprite.x, this.sprite.texture.textureCacheIds[0]);
     }
 }
