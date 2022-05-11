f87cea1422ec2cad4611014a35089df96907e0ba
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-05-12 03:34:15 +0400

    Add description file for generate objects of scene

--- src/loader.js
+++ src/loader.js
@@ -9,23 +9,23 @@ export class StoreTextures {
     build = () => new Promise((resolve, reject) => {
         this.loader
         .add([
-            {name: "block_blue", url: "assets/block_blue.png"},
-            {name: "block_green", url: "assets/block_green.png"},
-            {name: "block_purple", url: "assets/block_purple.png"},
-            {name: "block_red", url: "assets/block_red.png"},
-            {name: "block_yellow", url: "assets/block_yellow.png"},
-            {name: "bonus_bomb", url: "assets/bonus_bomb.png"},
-            {name: "bonus_change", url: "assets/bonus_change.png"},
-            {name: "bonus_full", url: "assets/bonus_full.png"},
-            {name: "game_field", url: "assets/game_field.png"},
+            {name: "blockBlue", url: "assets/block_blue.png"},
+            {name: "blockGreen", url: "assets/block_green.png"},
+            {name: "blockPurple", url: "assets/block_purple.png"},
+            {name: "blockRed", url: "assets/block_red.png"},
+            {name: "blockYellow", url: "assets/block_yellow.png"},
+            {name: "bonusBomb", url: "assets/bonus_bomb.png"},
+            {name: "bonusChange", url: "assets/bonus_change.png"},
+            {name: "bonusFull", url: "assets/bonus_full.png"},
+            {name: "gameField", url: "assets/game_field.png"},
             {name: "money", url: "assets/money.png"},
             {name: "pause", url: "assets/pause.png"},
-            {name: "progress_back", url: "assets/progress_back.png"},
-            {name: "progress_front", url: "assets/progress_front.png"},
+            {name: "progressBack", url: "assets/progress_back.png"},
+            {name: "progressFront", url: "assets/progress_front.png"},
             {name: "score", url: "assets/score.png"}
         ])
         .load(() => resolve())
     });
         
-    getTexture = (nameTexture) => this.loader.resources[nameTexture].texture;
+    getTexture = (name) => this.loader.resources[name].texture;
 }
