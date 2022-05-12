009635ccaf62fb05675eaa71ce68b2b45419c171
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-05-12 21:45:59 +0400

    Loading fonts in project

--- src/loader.js
+++ src/loader.js
@@ -1,4 +1,7 @@
 import { Loader } from "pixi.js";
+import { WebfontLoaderPlugin } from "pixi-webfont-loader";
+
+Loader.registerPlugin(WebfontLoaderPlugin);
 
 
 export class StoreTextures {
@@ -7,22 +10,26 @@ export class StoreTextures {
     }
 
     build = () => new Promise((resolve, reject) => {
+        let folderImg = "assets/images/";
+        let folderFont = "assets/fonts/";
+
         this.loader
         .add([
-            {name: "blockBlue", url: "assets/block_blue.png"},
-            {name: "blockGreen", url: "assets/block_green.png"},
-            {name: "blockPurple", url: "assets/block_purple.png"},
-            {name: "blockRed", url: "assets/block_red.png"},
-            {name: "blockYellow", url: "assets/block_yellow.png"},
-            {name: "bonusBomb", url: "assets/bonus_bomb.png"},
-            {name: "bonusChange", url: "assets/bonus_change.png"},
-            {name: "bonusFull", url: "assets/bonus_full.png"},
-            {name: "gameField", url: "assets/game_field.png"},
-            {name: "money", url: "assets/money.png"},
-            {name: "pause", url: "assets/pause.png"},
-            {name: "progressBack", url: "assets/progress_back.png"},
-            {name: "progressFront", url: "assets/progress_front.png"},
-            {name: "score", url: "assets/score.png"}
+            {name: "blockBlue", url: folderImg + "block_blue.png"},
+            {name: "blockGreen", url: folderImg + "block_green.png"},
+            {name: "blockPurple", url: folderImg + "block_purple.png"},
+            {name: "blockRed", url: folderImg + "block_red.png"},
+            {name: "blockYellow", url: folderImg + "block_yellow.png"},
+            {name: "bonusBomb", url: folderImg + "bonus_bomb.png"},
+            {name: "bonusChange", url: folderImg + "bonus_change.png"},
+            {name: "bonusFull", url: folderImg + "bonus_full.png"},
+            {name: "gameField", url: folderImg + "game_field.png"},
+            {name: "money", url: folderImg + "money.png"},
+            {name: "pause", url: folderImg + "pause.png"},
+            {name: "progressBack", url: folderImg + "progress_back.png"},
+            {name: "progressFront", url: folderImg + "progress_front.png"},
+            {name: "score", url: folderImg + "score.png"},
+            {name: "marvinFont", url: folderFont + "Marvin.ttf"}
         ])
         .load(() => resolve())
     });
